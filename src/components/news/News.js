ba1b6d84fba444e7beef4a604471adde35e9a3b6
iayaz1 <iamlordayaz@gmail.com>
2023-12-14 12:52:59 +0530

    Added Css for making cards appear side by side

--- src/components/news/News.js
+++ src/components/news/News.js
@@ -1,40 +1,35 @@
-import React, { useEffect, useState } from 'react'
-import './news.css'
+import React, { useEffect, useState } from 'react';
+import './news.css';
 
 const News = () => {
+  const [mynews, setMyNews] = useState([]);
 
-    const [mynews, setMyNews] = useState([]);
-    const fetchData = async ()=>{
-        const res = await fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=714ef9b8a6ef47d19b4bda6f4f0d100f")
-        const data = await res.json()
-       setMyNews(data.articles)
-    }
-    useEffect(()=>{
-        fetchData();
-    },[])
+  const fetchData = async () => {
+    const res = await fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=714ef9b8a6ef47d19b4bda6f4f0d100f");
+    const data = await res.json();
+    setMyNews(data.articles);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <>
-        {
-            mynews.map((ele)=>{
-                return(
-                    <>
-                    <div className='mainDiv'>
-                        <div class="card" style={{width: "18rem"}}>
-                        <img src={ele.urlToImage} class="card-img-top" alt="..."/>
-                        <div class="card-body">
-                            <h5 class="card-title">{ele.author}</h5>
-                            <p class="card-text">{ele.title}</p>
-                            <a href={ele.url} target='_blank' class="btn btn-primary">Read more</a>
-                        </div>
-                        </div>
-                    </div>
+    <div className="card-container">
+      {mynews.map((ele) => (
+        <div className="card" key={ele.title} style={{ width: "18rem" }}>
+          <img src={ele.urlToImage} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{ele.author}</h5>
+            <p className="card-text">{ele.title}</p>
+            <a href={ele.url} target='_blank' className="btn btn-primary">
+              Read more
+            </a>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-                    </>
-                )
-            })
-        }
-    </>
-  )
-}
-
-export default News
+export default News;
